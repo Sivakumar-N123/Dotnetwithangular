537f81decf479cf6bc32381d33cf283a37c7a9de
Jaya2Prakash <jayaprakash.r@qbrainx.com>
2022-09-07 10:55:49 +0530

    login page

--- dotnetwithangular/src/app/app-routing.module.ts
+++ dotnetwithangular/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CourseComponent } from './course/course.component';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+
 const routes: Routes = [
   {path:"course",component:CourseComponent},
- { path:"admin",component:AdminComponent}
+ { path:"admin",component:AdminComponent},
+ {path:"login",component:LoginComponent}
+ 
 ]
 
 
--- dotnetwithangular/src/app/app.module.ts
+++ dotnetwithangular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent,
-    AdminComponent
+    CourseComponent, 
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
