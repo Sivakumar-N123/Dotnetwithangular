a92d6ffd92ae7ce99b8be4ef35e33fb453f82050
mahima m <mahima.m@qbrainx.com>
2022-09-14 18:02:12 +0530

    data commit

--- dotnetwithangular/src/app/course/course.component.ts
+++ dotnetwithangular/src/app/course/course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { fakeAsync } from '@angular/core/testing';
+import { FormGroup,FormControl,FormControlName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-course',
@@ -7,23 +8,54 @@ import { fakeAsync } from '@angular/core/testing';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-
  
+   loginForm=new FormGroup({
+    id : new FormControl(''),
+    user :new FormControl('',[Validators.required]),
+   })
+   loginUser(){
+    console.warn(this.loginForm.value)
+   }
+  
+   get user(){
+    return this.loginForm.get('user');
+   }
+   
+   displayVal:String='';
+   getValue(val:String){
+      console.warn(val);
+      this.displayVal=val;
+   }
 
-   public gridData: any []= [
+
+
+
+
+
+  CourseDetails: any []= [
+    
     {
-      CourseID: 1,CourseName:"Chai"
+      CourseID: 1,CourseName:"Mahima"
     },
     {
-      CourseID: 2,CourseName:"Chang"
+      CourseID: 2,CourseName:"Aarthi"
     },
     {
-      CourseID: 3,CourseName:"Aniseed Syrup"
+      CourseID: 3,CourseName:"Shiva"
+    },
+    {
+      CourseID: 4,CourseName:"Rajesh"
+    },
+    {
+      CourseID: 5,CourseName:"JayaPrakash"
+    },
+    {
+      CourseID: 6,CourseName:"Preethi"
     },
   ];
 
-
   constructor() { 
+   
   }
   edited:boolean=false;
   deleted:boolean=false;
@@ -33,13 +65,23 @@ export class CourseComponent implements OnInit {
   header:boolean=true;
   body3:boolean=false;
 
+
   ngOnInit(): void {
   }
   public onButtonClick(): void {
     console.log("click");
-    alert("Do you want to delete");
+    alert("Deleted successfully....!");
   }
- 
+  public onButtonClick1(): void {
+    console.log("click");
+    alert("Added Successfully....!");
+    this.CourseDetails.push({CourseID: this.CourseDetails.length+1,CourseName:this.loginForm.value.user})
+
+  }
+  public onButtonClick2(): void {
+    console.log("click");
+    alert("Updated Successfully....!");
+  }
   adding(){
     this.body=true;
     this.body2=false;
@@ -47,6 +89,7 @@ export class CourseComponent implements OnInit {
     this.added=true;
     this.edited=false;
     this.deleted=false;
+    this.header=true;
   }
   update(){
    this.body=false;
@@ -54,15 +97,31 @@ export class CourseComponent implements OnInit {
    this.body3=true;
    this.edited=true;
    this.added=false;
-   this.deleted=false
+   this.deleted=false;
+   this.header=true;
   }
-  delete(){
-   this.deleted=true;
+
+  deleting(){
+    this.deleted=true;
    this.edited=false;
    this.added=false;
+   this.header=false;
    this.body2=true;
    this.body=false;
    this.body3=false;
+     
   }
- 
+  update1(){
+    this.loginForm.reset();
+      this.body=true;
+      this.body2=false;
+      this.body3=false;
+      this.added=true;
+      this.edited=false;
+      this.deleted=false;
+  }
+  
+
+
+
 }
