d77aaf7620b5b1de623b3bac6c2c9c6b19863821
Jaya2Prakash <jayaprakash.r@qbrainx.com>
2022-09-14 15:40:22 +0530

    validation changes

--- dotnetwithangular/src/app/login/login.component.ts
+++ dotnetwithangular/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute , Router} from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ButtonThemeColor } from '@progress/kendo-angular-buttons';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,51 +9,56 @@ import { ButtonThemeColor } from '@progress/kendo-angular-buttons';
 })
 export class LoginComponent implements OnInit {
 
-  stuform!:FormGroup;
-  option1 =false;
-  stu={pass:'',name:''};
-    
-  router:any;
+  stu={pass:'',username:''};
+  logform!:FormGroup;
+  submitted = false;
+
+   router:any;
   activateroute:any;
-  constructor(_r:Router,_a:ActivatedRoute) { 
-    this.router = _r;
+
+  constructor(private fb:FormBuilder,_r:Router,_a:ActivatedRoute)
+  {
+      this.router = _r;
     this.activateroute = _a;
   }
-
-  get pass() {
-    return this.stuform.get('pass');
-  }
-  get name() {
-    return this.stuform.get('name');
-  }
   ngOnInit(): void {
-    this.stuform = new FormGroup({
-           pass: new FormControl(this.stu.pass, [
-        Validators.minLength(8)
-      ]),
-      name: new FormControl(this.stu.name, [
-        Validators.required,
-        Validators.minLength(8),
-        Validators.maxLength(15),
-      ]),
-
-    });
+    this.logform = this.fb.group({
+      username:['',[Validators.required]],
+      pass:['',[Validators.required,Validators.minLength(8),Validators.maxLength(12)]]
+    })
   }
 
-  log()
-  {
-    // const checkbox = document.getElementById('subscribe',) as HTMLInputElement | null;
-    // if (checkbox?.checked)
-    // {
-    //   console.log('Checkbox is checked');
-    // }
-    // else
-    // {
-    //   console.log('Checkbox is NOT checked');
-    // }
-    
-    this.stu =this.stuform.value;
+  onsubmit(){
+    this.submitted = true;
+
+    const checkbox = document.getElementById('subscribe',) as HTMLInputElement | null;
+    if (checkbox?.checked)
+    {
+      console.log('Checkbox is checked');
+    }
+    else
+    {
+      console.log('Checkbox is NOT checked');
+    }
+
+      this.stu =this.logform.value;
     console.log(this.stu.pass);
-    console.log(this.stu.name);
-  } 
+    console.log(this.stu.username);
+
+    if(this.logform.invalid)
+    {
+      return;
+    }
+
+    alert("login successfully");
+    this.router.navigateByUrl("/update");
+  }
+
+  visible:boolean = true;
+  changetype:boolean =true;
+
+  viewpass(){
+    this.visible = !this.visible;
+    this.changetype = !this.changetype;
+  }
 }
--- dotnetwithangular/src/app/updatepwd/updatepwd.component.ts
+++ dotnetwithangular/src/app/updatepwd/updatepwd.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute , Router} from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-updatepwd',
@@ -7,10 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatepwdComponent implements OnInit {
 
-  constructor() { }
+  pwd!:FormGroup;
+  submitted1 = false;
 
+  stu1={oldpass:'',newpass:'',confirmpass:''};
+
+
+  constructor(private fb:FormBuilder)
+  {
+
+  }
   ngOnInit(): void {
+    this.pwd = this.fb.group({
+      
+      oldpass:['',[Validators.required,Validators.minLength(8),Validators.maxLength(12)]],
+      newpass:['',[Validators.required,Validators.minLength(8),Validators.maxLength(12)]],
+      confirmpass:['',[Validators.required,Validators.minLength(8),Validators.maxLength(12)]]
+    })
   }
 
-  
+  onsubmit(){
+    this.submitted1 = true;
+
+      this.stu1 =this.pwd.value;
+    console.log(this.stu1.oldpass);
+    console.log(this.stu1.newpass);
+    console.log(this.stu1.confirmpass);
+
+    if(this.pwd.invalid)
+    {
+      return;
+    }
+
+    alert("login successfully");
+ 
+  }
+
+  visible:boolean = true;
+  changetype:boolean =true;
+
+  viewpass1(){
+    this.visible = !this.visible;
+    this.changetype = !this.changetype;
+  }
+
+
+
+  visible1:boolean = true;
+  changetype1:boolean =true;
+
+  viewpass2(){
+    this.visible1 = !this.visible1;
+    this.changetype1 = !this.changetype1;
+  }
+
+  visible2:boolean = true;
+  changetype2:boolean =true;
+
+  viewpass3(){
+    this.visible2 = !this.visible2;
+    this.changetype2 = !this.changetype2;
+  }
 }
