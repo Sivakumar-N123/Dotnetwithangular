8d7973d142a51430ad04c67d4848205c9e5dd629
Arthiii <arthi.v@qbrainx.com>
2022-09-16 12:44:25 +0530

    Work done

--- dotnetwithangular/src/app/specification/specification.component.ts
+++ dotnetwithangular/src/app/specification/specification.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup,  Validators } from '@angular/forms';
 import { ButtonThemeColor } from "@progress/kendo-angular-buttons";
-import { Product } from "../model";
+
+interface userData{
+  id:Number,
+  name:string,
+  userName:string
+}
 
 @Component({
   selector: 'app-specification',
@@ -20,12 +25,22 @@ body1:boolean=false;
 title:boolean=true;
 title1:boolean=false;
 
-
 specForm!:FormGroup
 submitted =false;
 specName="";
+  
   constructor(private formBuilder:FormBuilder) { }
-
+ 
+  add1(){
+    this.specForm.reset()
+    this.footer=true
+    this.editfooter=false
+    this.footer1=false
+    this.body=true
+    this.body1=false
+    this.title=true
+    this.title1=false
+  }
   ngOnInit() {
     //validations
     this.specForm = this.formBuilder.group({
@@ -39,16 +54,22 @@ specName="";
     }
     else{
     alert("Saved Successfully")
+    let ref =document.getElementById('cancel')
+    ref?.click();
     this.gridData.push({ID: this.gridData.length+1,SpecificationName:this.specForm.value.specName})
+    this.specForm.reset()
     }
   }
+
   public gridData: any[] = [
     {ID: 1,SpecificationName: "EEE",},
     {ID: 2,SpecificationName: "ECE",},
     {ID: 3,SpecificationName: "IT",},
   ];
- 
-edit1(){
+
+  updateid: any;
+  edit1(row:any)
+  {
    this.editfooter=true
    this.footer=false
    this.footer1=false
@@ -56,35 +77,39 @@ edit1(){
    this.body1=false
    this.title=true
    this.title1=false
+   
+   console.log(row);
+   this.updateid=row.ID
+   this.editfooter=true;
+   this.specForm.controls['specName'].patchValue(row.SpecificationName);
+  }
+  update()
+  {
+    alert("Updated successfully")
+    if(this.gridData.length && this.specForm.value.specName){
+
+        this.gridData[this.updateid-1].SpecificationName=this.specForm.value.specName,
+        
+     
+      this.specForm.reset();
     }
-add1(){
-  this.specForm.reset()
-  this.footer=true
-  this.editfooter=false
-  this.footer1=false
-  this.body=true
-  this.body1=false
-  this.title=true
-  this.title1=false
-}
-delete1(){
-  this.footer=false
-  this.footer1=true
-  this.editfooter=false
-  this.body=false
-  this.body1=true
-  this.title=false
-   this.title1=true
-}
+  }
 
-save(){
-  alert("Saved successfully")
-}
-update(){
-  alert("Updated successfully")
-}
-yes(){
-  alert("Deleted successfully")
-}
+ delete1(row:any)
+  {
+    this.footer=false
+    this.footer1=true
+    this.editfooter=false
+    this.body=false
+    this.body1=true
+    this.title=false
+    this.title1=true
 
+    this.updateid=row.ID
+  }
+  remove()
+  { alert("Deleted successfully")
+    this.gridData.splice(this.updateid-1,1)
+  }
+  
 }
